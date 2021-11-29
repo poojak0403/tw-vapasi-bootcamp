292b42ee3ce70d889a82c53441c482239d1ec05e
pooja.keshri <pooja0403keshri@gmail.com>
2021-11-29 16:19:44 +0530

    pooja | add Rectangle

--- Rectangle/src/main/java/Rectangle.java
+++ Rectangle/src/main/java/Rectangle.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Rectangle {
+public class Rectangle {
+    private final int length;
+    private final int breadth;
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+
+    }
+
+    public int findArea() {
+
+        return length * breadth;
+    }
 }
--- Rectangle/src/test/java/RectangleTest.java
+++ Rectangle/src/test/java/RectangleTest.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class RectangleTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class RectangleTest {
+    @Test
+    void shouldReturnAreaOfRectangle() {
+
+        Rectangle rectangle = new Rectangle(20, 10);
+
+        int area = rectangle.findArea();
+
+        assertEquals(200, area);
+    }
 }
